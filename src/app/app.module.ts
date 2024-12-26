f2f14d00e5dcce634c89bdbc3775aad5067a6162
Batrahi Safaa <93432305+Safaamigo@users.noreply.github.com>
2024-12-27 00:16:51 +0100

    gen_code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,10 @@ import { ProjectsComponent } from './pages/projects/projects.component';
 // Ajoutez les modules d'Angular Material ici
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';  // Si vous utilisez des boutons Material
+import { MatButtonModule } from '@angular/material/button';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { GeneratecodeComponent } from './pages/gen-code/generatecode.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +23,8 @@ import { MatButtonModule } from '@angular/material/button';  // Si vous utilisez
     HomeComponent,
     LoginComponent,
     SignupComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    GeneratecodeComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +34,11 @@ import { MatButtonModule } from '@angular/material/button';  // Si vous utilisez
     // Ajoutez les modules Angular Material à l'importation
     MatPaginatorModule,  // Pagination
     MatInputModule,      // Barre de recherche
-    MatButtonModule      // Boutons Material (si nécessaire)
+    MatButtonModule ,     // Boutons Material (si nécessaire)
+    MatGridListModule,
+    BrowserModule,
+    MatGridListModule,  // Ajouter le module MatGridListModule
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pages/gen-code/generatecode.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generatecode',
+  templateUrl: './generatecode.component.html',
+  styleUrl: './generatecode.component.css'
+})
+export class GeneratecodeComponent {
+
+}
